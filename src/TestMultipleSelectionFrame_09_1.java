aecba17beef048d0cf9794e73da472d6044e1dff
Yasx73 <111445302+Yasx73@users.noreply.github.com>
2022-10-16 12:12:58 -0500

    Add files via upload
    Uploading code 9 and its test

---
+++ src/TestMultipleSelectionFrame_09_1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+//Yessenia Mora Esquivel
+// MultipleSelectionFrame test.
+public class TestMultipleSelectionFrame_09_1 {
+    public static void main(String[] args) {
+        MultipleSelectionFrame_09 multipleSelectionFrame_16 = new MultipleSelectionFrame_09();
+        multipleSelectionFrame_16.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        multipleSelectionFrame_16.setSize(350, 140);
+        multipleSelectionFrame_16.setVisible(true);
+    }
+}// end class MultipleSelectionTest
