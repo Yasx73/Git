0fb9d0189a68ea775d0384c25d9b954806149bd4
Yasx73 <111445302+Yasx73@users.noreply.github.com>
2022-10-16 12:22:01 -0500

    Add files via upload
    Uploading code 16 and its test

---
+++ src/TestFrameGridLayout_16_1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+// Test the FrameGridLayout
+//Yessenia Mora Esquivel
+public class TestFrameGridLayout_16_1 {
+    public static void main(String[] args) {
+        FrameGridLayout_16 frameGridLayout_16 = new FrameGridLayout_16();
+        frameGridLayout_16.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameGridLayout_16.setSize(300,200);
+        frameGridLayout_16.setVisible(true);
+    }
+}// end class DemoGridLayout
