9c0632d314cb17d641b73cf23496a54ce5e61c0e
Yasx73 <111445302+Yasx73@users.noreply.github.com>
2022-10-16 12:07:35 -0500

    Add files via upload
    Uploading code 6 and its test

---
+++ src/TestFrameButtonOption_06_1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+//Yessenia Mora Esquivel
+// OptionButtonFrame test
+public class TestFrameButtonOption_06_1 {
+    public static void main(String[] args) {
+        FrameButtonOption frameButtonOption = new FrameButtonOption();
+        frameButtonOption.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameButtonOption.setSize(300,100);
+        frameButtonOption.setVisible(true);
+    }
+} // end class OptionButtonTest
